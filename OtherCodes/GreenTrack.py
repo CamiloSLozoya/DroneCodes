dc788f13bf162c43cd9114baa0deb8a5a55b72a4
CamiloSLozoya <149712190+CamiloSLozoya@users.noreply.github.com>
2025-04-10 19:21:02 -0600

    Update GreenTrack.py

--- OtherCodes/GreenTrack.py
+++ OtherCodes/GreenTrack.py
@@ -9,21 +9,25 @@ VerdeBajo1=np.array([36,50,70],np.uint8)
 VerdeAlto1=np.array([89,255,255],np.uint8)
 
 while True:
-    ret,frame= captura.read()
-    if ret == True:
-        frameHSV=cv2.cvtColor(frame,cv2.COLOR_BGR2HSV) #Transformación de RGB a HSV
-        maskVerde=cv2.inRange(frameHSV,VerdeBajo1,VerdeAlto1) #Encuentra rangos indicados en la imagen, guarda en máscara
-        contornos,_=cv2.findContours(maskVerde, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        
-        for i in contornos:
-            area=cv2.contourArea(i)
-            if area > 2000:
-                nuevoContorno=cv2.convexHull(i)
-                cv2.drawContours(frame, [nuevoContorno], 0, (0,255,0),3 )
-        
-        cv2.imshow("Suavizado", frame)
-        if cv2.waitKey(1) & 0xFF == ord("s"):
-            break
+    if  captura.isOpened():
+        ret,frame= captura.read()
+        if ret == True:
+            frameHSV=cv2.cvtColor(frame,cv2.COLOR_BGR2HSV) #Transformación de RGB a HSV
+            maskVerde=cv2.inRange(frameHSV,VerdeBajo1,VerdeAlto1) #Encuentra rangos indicados en la imagen, guarda en máscara
+            contornos,_=cv2.findContours(maskVerde, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            
+            for i in contornos:
+                area=cv2.contourArea(i)
+                if area > 2000:
+                    nuevoContorno=cv2.convexHull(i)
+                    cv2.drawContours(frame, [nuevoContorno], 0, (0,255,0),3 )
+            
+            cv2.imshow("Suavizado", frame)
+            if cv2.waitKey(1) & 0xFF == ord("s"):
+                break
+    else: 
+        print("Error: no se pudo abrir la cámara o el video.")
+        exit()
 
 captura.realease()
 cv2.destroyAllWindows()
