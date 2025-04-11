9b0ee9e239eaacaa3154a4f9aa46361a708b8070
CamiloSLozoya <149712190+CamiloSLozoya@users.noreply.github.com>
2025-04-10 18:12:05 -0600

    a

--- Test/Test1.py
+++ Test/Test1.py
@@ -3,55 +3,58 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time 
 
-# Toma de video con cámara
-captura = cv2.VideoCapture(0)
-
 # Rangos del color a detectar (verde en HSV)
 VerdeBajo1 = np.array([36, 50, 70], np.uint8)
-VerdeAlto1 = np.array([89, 255, 255], np.uint8)
+VerdeAlto1 = np.array([89, 255, 255], np.uint8) 
 
-# Valores de ExG mínimo y máximo
-EXG_MIN = 21  
-EXG_MAX = 137   
+#Cargar imagen
+img = cv2.imread('Test\imagen.jpg')
 
-while True:
-    ret, frame = captura.read()
-    if ret:
-        frameHSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        maskVerde = cv2.inRange(frameHSV, VerdeBajo1, VerdeAlto1)
-        maskVerdeRGB=cv2.cvtColor(maskVerde, cv2.COLOR_HSV2BGR)
-        contornos, _ = cv2.findContours(maskVerde, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        
-        for i in contornos:
+#Mostrar imagen
+if img is None:
+    #Marcar error si no hay imagen
+    print("Image not loaded. Check the path!")
+    exit()
+else:
+    #Convertir imagen a escala HSV
+    frameHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    #Usar mascara para deterctr los pixeles verdes
+    maskVerde = cv2.inRange(frameHSV, VerdeBajo1, VerdeAlto1)
+    #detectar los contornos
+    contornos, _ = cv2.findContours(maskVerde, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    #Separar la imagen en sus componentes RGB
+    b, g, r = cv2.split(img.astype("float"))
+    #dibujar los contornos
+    for i in contornos:
             area = cv2.contourArea(i)
             if area > 2000:
                 nuevoContorno = cv2.convexHull(i)
-                cv2.drawContours(frame, [nuevoContorno], 0, (0, 255, 0), 3)
+                cv2.drawContours(img, [nuevoContorno], 0, (0, 255, 0), 3)
+    #Imprimir imagen con contornos
+    cv2.imshow("Contornos de verde", img)
 
-        # -------------------------------
-        # Cálculo de índice ExG (similar a NDVI)
-        # -------------------------------
-        b, g, r = cv2.split(frame.astype("float"))
-        exg = 2 * g - r - b  # Fórmula del ExG
+    # -------------------------------
+    # Cálculo de índice ExG (similar a NDVI)
+    # -------------------------------
+    exg = 2 * g - r - b  # Fórmula del ExG
+    exg_norm = cv2.normalize(exg, None, 0, 255, cv2.NORM_MINMAX).astype("uint8") #Normalizar imagen
+    exg_mascarado = cv2.bitwise_and(exg_norm, exg_norm, mask=maskVerde) #Eliminar pixeles que no sean verdes
+    #Imprimir imagen verde
+    cv2.imshow("Imagen ExG normalizada", exg_norm)
 
-        # Aplicar la máscara para ExG solo en las zonas verdes
-        exg_mascarado = cv2.bitwise_and(exg, exg, mask=maskVerdeRGB)
-        valores_validos = exg_mascarado[maskVerde > 0]
+    #Calcular porcentaje de verdor
+    pixeles_totales = maskVerde.shape[0] * maskVerde.shape[1]
+    pixeles_verdes = cv2.countNonZero(maskVerde)
+    porcentaje_verde = (pixeles_verdes / pixeles_totales) * 100 #porcentaje de pixeles verdes en la imagen
+    porcentaje_verdor = (np.sum(exg_mascarado)/(pixeles_verdes*255))*100 #porcentaje de verdor de los pixeles verdes
 
-        if len(valores_validos) > 0:
-            promedio_exg = np.mean(valores_validos)
-            porcentaje_verdor = (promedio_exg - EXG_MIN) / (EXG_MAX - EXG_MIN) * 100
-            print(f"Porcentaje de verdor: {porcentaje_verdor: }%")
-        else:
-            porcentaje_verdor = 0
-            print("No se detectó zona verde")
+    #Imprimir resultado
+    print(f"Porcentaje de pixeles verdes: {porcentaje_verde:.2f}%")
+    print(f"Porcentaje de verdor: {porcentaje_verdor:.2f}%")
 
-        cv2.imshow("Suavizado", frame)
-
-        if cv2.waitKey(1) & 0xFF == ord("s"):
-            break
-
-captura.release()
+#Espera a que preciones la letra s para cerrar y salir del código
+while True:
+    if cv2.waitKey(1) & 0xFF == ord("s"):
+         break
+    
 cv2.destroyAllWindows()
-
-#meterle for
