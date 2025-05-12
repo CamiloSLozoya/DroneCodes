e8fb990158f4f3086b8910ac0f9e1d7e673947a1
Brucelealrx <90417160+Brucelealrx@users.noreply.github.com>
2025-05-11 20:29:41 -0600

    added pad

--- TKInterface.py
+++ TKInterface.py
@@ -2,4 +2,10 @@ import tkinter as tk
 
 root = tk.Tk()
 
+root.geometry("500x500")
+root.title("Flight Monitor")
+
+label = tk.label(root,text="Altura",font=('Comic Sans',18))
+label.pack(padx=20,pady=20)
+
 root.mainloop()
